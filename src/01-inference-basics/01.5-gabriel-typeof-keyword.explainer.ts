1c8d24fec50fa6711448e678d6349115d317f39c
Sarah Diethert <sarah.diethert@sd-c.de>
2024-04-30 13:52:32 +0200

    01.01. explainer

--- src/01-inference-basics/01.5-gabriel-typeof-keyword.explainer.ts
+++ src/01-inference-basics/01.5-gabriel-typeof-keyword.explainer.ts
@@ -1,3 +1,8 @@
 // Gabriel Vergnaud explains the typeof keyword, and the type/value level
 
-export {};
+// typeof takes entities from the value level and takes it to the type level
+// only in that direction
+// synonym = type inference
+
+export { };
+
