a4fd7dabba8f506f2f2cfb6d8fc8f8d353f18b15
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-02 15:47:52 +0200

    02.09.

--- src/02-unions-and-indexing/09-discriminated-union-to-discriminator.problem.ts
+++ src/02-unions-and-indexing/09-discriminated-union-to-discriminator.problem.ts
@@ -14,6 +14,12 @@ export type Event =
       event: KeyboardEvent;
     };
 
-type EventType = unknown;
+/**
+ * when you access a key on a union you access all of the possible permutations of that key
+ * this only works when all entities of a union have the key that is being accessed
+ *
+ * indexed access can be used when the key belongs to each member of the union
+ */
+type EventType = Event["type"];
 
 type tests = [Expect<Equal<EventType, "click" | "focus" | "keydown">>];
