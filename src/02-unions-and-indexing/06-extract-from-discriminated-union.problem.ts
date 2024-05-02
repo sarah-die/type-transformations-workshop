563949b22acef1f9885ab688dcd84801b4708b18
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-02 11:09:26 +0200

    02.06.

--- src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
+++ src/02-unions-and-indexing/06-extract-from-discriminated-union.problem.ts
@@ -6,14 +6,21 @@ export type Event =
       event: MouseEvent;
     }
   | {
-      type: "focus";
-      event: FocusEvent;
-    }
-  | {
       type: "keydown";
       event: KeyboardEvent;
     };
 
-type ClickEvent = unknown;
+// utility type
+// constructs a type by extracting from Type
+type ClickEvent = Extract<Event, { type: "click" }>;
+type ClickEvent2 = Extract<Event, { event: MouseEvent }>;
+
+// note: Extract returns all the objects that fits the checked as a union
+
+type Fruit = "banana" | "apple" | "orange";
+type BananaAndOrange = Extract<Fruit, "banana" | "orange">;
 
 type tests = [Expect<Equal<ClickEvent, { type: "click"; event: MouseEvent }>>];
+type tests2 = [
+  Expect<Equal<ClickEvent2, { type: "click"; event: MouseEvent }>>
+];
