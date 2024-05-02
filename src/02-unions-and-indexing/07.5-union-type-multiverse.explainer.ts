d4363c53742913199f53e14b9e90b1df472f5d78
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-02 11:27:05 +0200

    02.07.5.

--- src/02-unions-and-indexing/07.5-union-type-multiverse.explainer.ts
+++ src/02-unions-and-indexing/07.5-union-type-multiverse.explainer.ts
@@ -1,3 +1,21 @@
 // Gabriel Vergnaud explains his idea of the union type multiverse
 
 export {};
+
+/**
+ * what is the JS equivalent of a union type?
+ * union types represent different possibilities your code might handle over time
+ *
+ * example: go to the office -> be on time or grab a coffee -> be late
+ * TS: all of these possibilities exist simultaneously
+ * like having a multiverse and everything that can happen happens
+ * when defining a union type you create a multiverse
+ * since everything can happen we have to evaluate all paths that are defined due to the union
+ *
+ *
+ * TLDR:
+ * union types represent the different possibilities in your code
+ * the different possibilities the code cares about on the type level
+ * at runtime the code can be run with lots of different values
+ * all of those possible branches have to be defined
+ * */
