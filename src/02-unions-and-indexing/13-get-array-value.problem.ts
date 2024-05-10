44f8c6e7f5b890974602a504716ca92c10b40e0a
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 11:19:08 +0200

    02.13.

--- src/02-unions-and-indexing/13-get-array-value.problem.ts
+++ src/02-unions-and-indexing/13-get-array-value.problem.ts
@@ -1,11 +1,19 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fruits = ["apple", "banana", "orange"];
+// as const = all elements are typed as their literal values
+// fruits should not be string[]
+const fruits = ["apple", "banana", "orange"] as const;
 
-type AppleOrBanana = unknown;
-type Fruit = unknown;
+type FruitKeys = typeof fruits;
+
+// indexed access type into the array of types
+// note: indexed access can be used with array indexes
+type AppleOrBanana = FruitKeys[0 | 1]; // "apple" | "banana"
+
+// [number] will work for all elements of the array and returns them as a union
+type Fruit = FruitKeys[number]; // "apple" | "banana" | "orange"
 
 type tests = [
   Expect<Equal<AppleOrBanana, "apple" | "banana">>,
-  Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
+  Expect<Equal<Fruit, "apple" | "banana" | "orange">>
 ];
