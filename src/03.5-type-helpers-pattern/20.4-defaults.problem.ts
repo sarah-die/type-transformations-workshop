953bf47cd2c13c2a98e2582e82fb45a3dbc9f5f4
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 16:27:43 +0200

    03.5.20.4.

--- src/03.5-type-helpers-pattern/20.4-defaults.problem.ts
+++ src/03.5-type-helpers-pattern/20.4-defaults.problem.ts
@@ -1,6 +1,15 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type CreateDataShape<TData, TError> = {
+// argument does not have to be passed
+// if it is not passed it defaults to the given value
+// note: optional type params have to be the last ones
+type CreateDataShape<TData, TError = undefined> = {
+  data: TData;
+  error: TError;
+};
+
+type MaybeError = Error | undefined;
+type CreateDataShape2<TData, TError extends MaybeError = undefined> = {
   data: TData;
   error: TError;
 };
@@ -23,5 +32,5 @@ type tests = [
         error: SyntaxError;
       }
     >
-  >,
+  >
 ];
