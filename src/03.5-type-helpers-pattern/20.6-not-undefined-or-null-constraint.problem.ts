faf2533c9f325b914db5b57d567e6651567de3ff
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 17:06:10 +0200

    03.5.20.6.

--- src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
+++ src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
@@ -1,4 +1,12 @@
-export type Maybe<T> = T | null | undefined;
+// outcome: should be anything other then null and undefined
+export type Maybe<T extends {}> = T | undefined | null;
+
+// empty object in TS represent everything that is not null or undefined
+// Reason: TS does structural comparisons when checking that things are assignable to each other
+// an object with keys and types is assignable to an empty oject
+// string, number & co: are just objects with various methods associated with it in TS
+// TS: everything is an object except for null and undefined
+// null/ undefined: has no methods/ no properties/ no props can be assigned
 
 type tests = [
   // @ts-expect-error
@@ -9,5 +17,5 @@ type tests = [
   Maybe<string>,
   Maybe<false>,
   Maybe<0>,
-  Maybe<"">,
+  Maybe<"">
 ];
