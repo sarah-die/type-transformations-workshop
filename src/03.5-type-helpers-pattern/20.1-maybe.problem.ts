55b9f2a173e1b8aac5bca47d6f7fdd62304e9fa6
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 14:46:06 +0200

    03.5.20.1.

--- src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
+++ src/03.5-type-helpers-pattern/20.1-maybe.problem.ts
@@ -1,10 +1,11 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type Maybe = unknown;
+type Maybe<T> = T | null | undefined;
+// note: two union members of the same type get squashed together
 
 type tests = [
   Expect<Equal<Maybe<string>, string | null | undefined>>,
   Expect<Equal<Maybe<number>, number | null | undefined>>,
   Expect<Equal<Maybe<boolean>, boolean | null | undefined>>,
-  Expect<Equal<Maybe<null>, null | undefined>>,
+  Expect<Equal<Maybe<null>, null | undefined>>
 ];
