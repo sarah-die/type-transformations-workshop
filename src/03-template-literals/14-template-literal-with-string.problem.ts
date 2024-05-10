ee564cf3975043ee306363df2bbd885c415344cc
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 11:49:59 +0200

    03.14.

--- src/03-template-literals/14-template-literal-with-string.problem.ts
+++ src/03-template-literals/14-template-literal-with-string.problem.ts
@@ -1,15 +1,18 @@
-type Route = unknown;
+type Route = `/${string}`;
 
+// any string that starts with "/" is allowed
 export const goToRoute = (route: Route) => {};
 
-// Should succeed:
+const test = "anyThing";
+const test2: Route = "/anyThing";
 
-goToRoute("/users");
+// Should succeed:
+goToRoute(`/${test}`);
+goToRoute(test2);
 goToRoute("/");
 goToRoute("/admin/users");
 
 // Should error:
-
 // @ts-expect-error
 goToRoute("users/1");
 // @ts-expect-error
