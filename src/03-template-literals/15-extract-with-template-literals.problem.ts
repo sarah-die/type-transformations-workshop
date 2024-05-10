97737cb0c4fbd13f0f349f9abc9a90663f9fe416
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 11:57:35 +0200

    03.15.

--- src/03-template-literals/15-extract-with-template-literals.problem.ts
+++ src/03-template-literals/15-extract-with-template-literals.problem.ts
@@ -2,6 +2,7 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Routes = "/users" | "/users/:id" | "/posts" | "/posts/:id";
 
-type DynamicRoutes = unknown;
+// like regex = allows TS to dive deep into the strings to understand all different parts of them
+type DynamicRoutes = Extract<Routes, `${string}/:${string}`>;
 
 type tests = [Expect<Equal<DynamicRoutes, "/users/:id" | "/posts/:id">>];
