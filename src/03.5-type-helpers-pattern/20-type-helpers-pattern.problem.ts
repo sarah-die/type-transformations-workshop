e9e8ae556acec86f629317e40fbab48b5daf5c58
Sarah Diethert <sarah.diethert@sd-c.de>
2024-05-10 14:35:19 +0200

    03.5.20.

--- src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
+++ src/03.5-type-helpers-pattern/20-type-helpers-pattern.problem.ts
@@ -1,11 +1,16 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type ReturnWhatIPassIn = unknown;
+// generics (here: type helpers)
+// Allows to create functions that return other types
+// function<arg of the function> = function return
+type ReturnWhatIPassIn<T> = T;
+
+type ReturnTest = ReturnWhatIPassIn<"test">;
 
 type tests = [
   Expect<Equal<ReturnWhatIPassIn<1>, 1>>,
   Expect<Equal<ReturnWhatIPassIn<"1">, "1">>,
   Expect<Equal<ReturnWhatIPassIn<true>, true>>,
   Expect<Equal<ReturnWhatIPassIn<false>, false>>,
-  Expect<Equal<ReturnWhatIPassIn<null>, null>>,
+  Expect<Equal<ReturnWhatIPassIn<null>, null>>
 ];
