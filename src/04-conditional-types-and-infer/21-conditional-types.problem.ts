a4486ebb2f54f161b80f3ff04d8cb403a41ebeb0
Sarah Diethert <sarah.diethert@sd-c.de>
2024-06-03 15:10:08 +0200

    04.21.

--- src/04-conditional-types-and-infer/21-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/21-conditional-types.problem.ts
@@ -1,8 +1,12 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type YouSayGoodbyeAndISayHello = unknown;
+// add a type argument T
+// T turns this from a static value to a function
+// "extends" checks if it matches a pattern
+// TS 4.8 only the ternary syntax is possible
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
 
 type tests = [
   Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
-  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>,
+  Expect<Equal<YouSayGoodbyeAndISayHello<"goodbye">, "hello">>
 ];
