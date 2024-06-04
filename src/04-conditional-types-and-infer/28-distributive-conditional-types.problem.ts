72b719573044d9508928f4f4f442a3d81ee325da
Sarah Diethert <sarah.diethert@sd-c.de>
2024-06-04 14:31:23 +0200

    04.28.

--- src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
+++ src/04-conditional-types-and-infer/28-distributive-conditional-types.problem.ts
@@ -2,6 +2,37 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Fruit = "apple" | "banana" | "orange";
 
-type AppleOrBanana = Fruit extends "apple" | "banana" ? Fruit : never;
+// not working because it´s not used in a generic context
+// so the entire type Fruit is compared with the entire extends term
+// it contains to much so the result is never
+type AppleOrBanana0 = Fruit extends "apple" | "banana"
+  ? "apple" | "banana"
+  : never;
+
+//@ts-expect-error
+type tests0 = [Expect<Equal<AppleOrBanana0, "apple" | "banana">>];
+
+// first solution: without generic
+// infer what is in the Fruit
+// now TS will iterate over it, because it is in a generic context
+// T is iterable
+type AppleOrBanana = Fruit extends infer T
+  ? T extends "apple" | "banana"
+    ? T
+    : never
+  : never;
 
 type tests = [Expect<Equal<AppleOrBanana, "apple" | "banana">>];
+
+// second solution: with generic
+// when using a generic the members of the union distribute across it
+// T represents each individual member
+// T "iterates" trough the members of the union and checks the condition for all of them
+type GetAppleOrBanana2<T> = T extends "apple" | "banana" ? T : never;
+type AppleOrBanana2 = GetAppleOrBanana2<Fruit>;
+
+type tests2 = [Expect<Equal<AppleOrBanana2, "apple" | "banana">>];
+
+// issue: distributive conditional types
+// distributive = verteilend
+// attributes are distributed into an union
