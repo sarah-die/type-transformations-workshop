2f23d24e7f4ece5e727f02c77b4a627bc0c68ab9
Sarah Diethert <sarah.diethert@sd-c.de>
2024-06-04 13:17:46 +0200

    04.26.

--- src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
+++ src/04-conditional-types-and-infer/26-get-result-from-async-function.problem.ts
@@ -10,12 +10,44 @@ const getServerSideProps = async () => {
   };
 };
 
-type InferPropsFromServerSideFunction = unknown;
+// conditional check + infer
+// grab out the right thing (props) and return it
+// otherwise return never
+
+// my solution
+type InferPropsFromServerSideFunction<T> = T extends () => infer R
+  ? Awaited<R> extends { props: infer TProps2 }
+    ? TProps2
+    : never
+  : never;
+
+type TypeOfX = typeof getServerSideProps;
+type ReturnTypeX = ReturnType<TypeOfX>;
+type PromiseX = Awaited<ReturnTypeX>;
+type Example = InferPropsFromServerSideFunction<typeof getServerSideProps>;
+
+// Matts solution
+// T should extend a function that returns a promise
+// inside that promise should be an object
+type InferPropsFromServerSideFunction1<T> = T extends () => Promise<{
+  props: infer TProps;
+}>
+  ? TProps
+  : never;
 
 type tests = [
   Expect<
     Equal<
       InferPropsFromServerSideFunction<typeof getServerSideProps>,
+      { json: { title: string } }
+    >
+  >
+];
+
+type tests1 = [
+  Expect<
+    Equal<
+      InferPropsFromServerSideFunction1<typeof getServerSideProps>,
       { json: { title: string } }
     >
   >
