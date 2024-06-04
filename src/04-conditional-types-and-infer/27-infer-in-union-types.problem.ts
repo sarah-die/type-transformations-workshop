c5d6df6d3d7d75613c8ea22a2354910f3424d337
Sarah Diethert <sarah.diethert@sd-c.de>
2024-06-04 13:58:38 +0200

    04.27.

--- src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
+++ src/04-conditional-types-and-infer/27-infer-in-union-types.problem.ts
@@ -10,10 +10,25 @@ const parser3 = {
   extract: () => true,
 };
 
-type GetParserResult<T> = unknown;
+// my solution
+type GetParserResult<T> = T extends () => infer TReturn
+  ? TReturn
+  : T extends { parse: () => infer TProps } | { extract: () => infer TProps }
+  ? TProps
+  : never;
+
+// Matts solution: use an union type for all three
+// if T extends any of these in the union type, take the results and return them
+// good to avoid long ternaries
+type GetParserResult1<T> = T extends
+  | { parse: () => infer TResult }
+  | (() => infer TResult)
+  | { extract: () => infer TResult }
+  ? TResult
+  : never;
 
 type tests = [
   Expect<Equal<GetParserResult<typeof parser1>, number>>,
   Expect<Equal<GetParserResult<typeof parser2>, string>>,
-  Expect<Equal<GetParserResult<typeof parser3>, boolean>>,
+  Expect<Equal<GetParserResult<typeof parser3>, boolean>>
 ];
